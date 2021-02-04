653c5fed9e6a7422325e836b6ed501137eb626f1
= <navneil2006@gmail.com>
2021-02-04 22:41:23 +1100

    ADD the boot sector prints Hello to the Screen:

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,16 @@
-; Infinite loop (e9 fd ff)
-loop:
-    jmp loop 
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10 ; 'l' is still on al, remember?
+mov al, 'o'
+int 0x10
 
-; Fill with 510 zeros minus the size of the previous code
-times 510-($-$$) db 0
-; Magic number
+jmp $ ; jump to current address = infinite loop
+
+; padding and magic number
+times 510 - ($-$$) db 0
 dw 0xaa55 
